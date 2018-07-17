ee498038a9bde533a26e4f47e106541f1a4bb221
Karl Rößler <karl.roessler@comparex.com>
2018-07-17 12:13:00 +0200

    Initial

---
+++ NewWordCount/NewWordCount/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewWordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
